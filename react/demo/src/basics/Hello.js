242ea2a78c7dad6b8774a45e16601426cf926dd5
srikanthpragada <srikanthpragada@gmail.com>
2022-12-15 17:56:39 +0530

    15-DEC

---
+++ react/demo/src/basics/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hello() {
+    var product = "React";
+    return (
+        <>
+            <h1>Hello {product.toUpperCase()}</h1>
+            <h3>Wonderful library for UI</h3>
+        </>
+    )
+}
